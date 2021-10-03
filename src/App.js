acfce39035d0fadf5dbc83732cd080906aa82aba
JoshDeeNc <61564763+JoshDeeNc@users.noreply.github.com>
2021-10-04 07:53:06 +1100

    Added react semantic UI package with some test code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Icon } from 'semantic-ui-react'
+
 function App() {
   return (
     <div>
+      G'day Folks
+      <Button size='small' color='green'>
+        <Icon name='download' />
+        Download
+      </Button>
     </div>
   );
 }
