6217b1bc2488b7ca7af9bdc248f87ee334bba86f
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2021-10-05 22:09:55 +1100

    Make ESLint happy

--- server/src/db.ts
+++ server/src/db.ts
@@ -4,8 +4,11 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 const initDB = async () => {
-  await mongoose.connect(
-    process.env.MONGO_URI)
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI is not defined');
+  }
+
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default initDB;
